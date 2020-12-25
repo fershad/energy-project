8a9cafd6353125219b71820fa76b04039c175ca1
fishintaiwan <fershad@gmail.com>
2020-12-25 12:05:53 +0800

    setup rollup and postcss

---
+++ rollup.config.js
@@ -0,0 +1,75 @@
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import svg from 'rollup-plugin-svg';
+import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+import svelte from 'rollup-plugin-svelte';
+
+const dev = process.env.NODE_ENV !== 'production';
+export default [
+    // {
+    //     input: 'src/_bundle/index.js',
+    //     output: [
+    //         {
+    //             sourcemap: false,
+    //             format: 'iife',
+    //             name: 'index',
+    //             file: 'dist/assets/index.bundle.legacy.js',
+    //         },
+    //         {
+    //             sourcemap: false,
+    //             format: 'es',
+    //             name: 'index',
+    //             file: 'dist/assets/index.bundle.js',
+    //         },
+    //     ],
+    //     plugins: [
+    //         replace({
+    //             DEV_MODE: dev,
+    //         }),
+    //         json(),
+    //         svelte({
+    //             dev,
+    //         }),
+    //         svg(),
+
+    //         resolve({
+    //             browser: true,
+    //             dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/'),
+    //         }),
+    //         commonjs(),
+    //         !dev && terser(),
+    //     ],
+    //     watch: {
+    //         clearScreen: false,
+    //     },
+    // },
+    {
+        input: 'src/_bundle/css.js',
+        output: [
+            {
+                file: '.cache/css.js',
+            },
+        ],
+        plugins: [
+            replace({
+                DEV_MODE: dev,
+            }),
+            /* Generates a cached file with all CSS,
+        /  which will be used later to purge & add only the CSS used
+        /  on each individual page. */
+            postcss({
+                extract: '.cache/main.css',
+                // inject: true,
+                // minimize: !dev,
+            }),
+            commonjs(),
+            dev && terser(),
+        ],
+        watch: {
+            clearScreen: false,
+        },
+    },
+];
