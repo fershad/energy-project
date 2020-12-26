2a1ca318d2a1b96c2d400d999c2153259d7fa12a
fishintaiwan <fershad@gmail.com>
2020-12-26 19:13:06 +0800

    adjust padding

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 const autoprefixer = require('autoprefixer');
+const postcssImport = require('postcss-import');
+const nesting = require('postcss-nesting');
 
 module.exports = {
-    plugins: [autoprefixer, require('postcss-import'), require('postcss-nesting')],
+    plugins: [autoprefixer, postcssImport, nesting],
 };
