8467d8cfbdd1813e6d0a695f9193c338b61d3756
fishintaiwan <fershad@gmail.com>
2021-01-04 08:43:38 +0800

    simple news page setup

---
+++ src/_data/page/news.js
@@ -0,0 +1,12 @@
+const intro = {
+    heading: {
+        en: `News & articles`,
+        zh: `新聞和文章`,
+    },
+    paragraph: {
+        en: `A collection of news, blog posts & articles in English/Chinese about the situation in Taiwan. Find more in the <a href="/en/news">news archive</a>.`,
+        zh: `有關台灣局勢的一些新聞，博客和文章 (中文 / 英文的). 看更多在 <a href="/zh/news">新聞檔案</a>.`,
+    },
+};
+
+module.exports = { intro };
