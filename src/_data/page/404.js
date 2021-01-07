4d625522021f1b61c8113afa6f784eb7edb6a38d
fishintaiwan <fershad@gmail.com>
2021-01-07 17:19:19 +0800

    add simple 404 page

---
+++ src/_data/page/404.js
@@ -0,0 +1,10 @@
+const intro = {
+    heading: {
+        en: 'Page not found',
+        zh: '',
+    },
+    paragraph: {
+        en: `Sorry but the page you're looking for can't be found.`,
+        zh: '',
+    },
+};
