b462b4cdb560903b034666f027f5701dd0a89fee
fishintaiwan <fershad@gmail.com>
2021-01-07 16:29:18 +0800

    start creating FAQ page

---
+++ src/_data/page/faq.js
@@ -0,0 +1,28 @@
+const intro = {
+    heading: {
+        en: 'FAQ',
+        zh: '',
+    },
+    paragraph: {
+        en: `If you've got any questions about the data or content of this website then you might find an answer here. If you can't find what you're looking for then <a href="mailto:cleanenergyproject@fershad.com">drop me an email</a> and I'll see if I can help.`,
+        zh: '',
+    },
+};
+
+const questions = [
+    {
+        question: {
+            en: `What energy sources considered 'clean'?`,
+            zh: ``,
+        },
+        answer: {
+            en: `<p>For this project I have considered wind, solar, hydro, nuclear, and geothermal as clean energy sources.
+            These sources generate energy don’t produce air pollutants or greenhouse gases as part of the energy generation process.</p>
+            <p>Taiwan also generates a marginal amount of electricity from burning biomass and waste, which are renewable energy sources.
+            However I currently don't have enough knowledge around how Taiwan handles either of these to feel comfortable that they are non-polluting.</p>`,
+            zh: ``,
+        },
+    },
+];
+
+module.exports = { intro, questions };
