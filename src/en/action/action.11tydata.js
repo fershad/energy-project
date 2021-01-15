b3a41f550300b07d3f4f7986b145334e284a60fb
fishintaiwan <fershad@gmail.com>
2021-01-15 14:35:46 +0800

    add image for action pages

--- src/en/action/action.11tydata.js
+++ src/en/action/action.11tydata.js
@@ -1,5 +1,6 @@
 module.exports = {
     tags: ['action', 'en'],
+    layout: 'pages/action-page',
     eleventyComputed: {
         permalink: '/{{ locale }}/actions/{{ shortName | slug }}/index.html',
         translationKey: 'action-{{ page.fileSlug }}',
