1df061c0608e9e11eb351515a987aa0acfdaf492
fishintaiwan <fershad@gmail.com>
2021-01-22 13:59:32 +0800

    add analytics

--- src/_11ty/shortcode/analytics.js
+++ src/_11ty/shortcode/analytics.js
@@ -1,14 +1,14 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = async function() {
-    if (dev) {
-        return `<!-- Fathom - beautiful, simple website analytics -->`;
-    }
-
     if (process.env.NODE_ENV === 'staging') {
         return `<!-- Fathom - beautiful, simple website analytics -->
       <script src="https://heron.cleanenergyproject.tw/script.js" data-site="TMLDWNIR" defer></script>
       <!-- / Fathom -->`;
+    }
+
+    if (dev) {
+        return `<!-- Fathom - beautiful, simple website analytics -->`;
     }
 
     return `<!-- Fathom - beautiful, simple website analytics --><script src="https://heron.cleanenergyproject.tw/script.js" data-site="DTNNQUEE" defer></script><!-- / Fathom -->`;
