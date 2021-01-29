0e4bd65f1c1adb68629f390469b24b7685f3544e
fishintaiwan <fershad@gmail.com>
2021-01-29 17:48:48 +0800

    add fathom goals

--- src/_bundle/_svelte/data/emissions.svelte
+++ src/_bundle/_svelte/data/emissions.svelte
@@ -1,34 +1,35 @@
 <script>
-    import { locale } from '../store'
-    let thisChart;
-    
-    let display = false;
-    let text = $locale === 'zh' ? '顯示圖表 ' : 'Show graph'
+  import { locale } from "../store";
+  let thisChart;
 
-    const toggleClass = () => {
-        if (thisChart === null || thisChart === '' || thisChart === undefined) {
-                thisChart = document.getElementById('emissionsChart');
-            }
-            thisChart.classList.toggle('hidden');
+  let display = false;
+  let text = $locale === "zh" ? "顯示圖表 " : "Show graph";
+
+  const toggleClass = () => {
+    if (thisChart === null || thisChart === "" || thisChart === undefined) {
+      thisChart = document.getElementById("emissionsChart");
     }
+    thisChart.classList.toggle("hidden");
+  };
 
-    async function showChart() {
-        display = !display;
-        if (display) {
-            text = $locale === 'zh' ? '隱藏圖表 ' : 'Hide graph'
-        } else {
-            text = $locale === 'zh' ? '顯示圖表 ' : 'Show graph'
-        }
-        toggleClass();
-
+  async function showChart() {
+    display = !display;
+    if (display) {
+      text = $locale === "zh" ? "隱藏圖表 " : "Hide graph";
+    } else {
+      text = $locale === "zh" ? "顯示圖表 " : "Show graph";
     }
+    toggleClass();
+  }
 
-    async function importChart() {
-        const { default: chart } = await import(
-                "/assets/emissionsChart.bundle.js"
-            );
-    }
+  async function importChart() {
+    const { default: chart } = await import("/assets/emissionsChart.bundle.js");
+    window.fathom.trackGoal("IA2NFGAJ", 0);
+  }
 </script>
 
-<button class="link--button" on:click={showChart} on:click|once={importChart}>{ text }</button>
-
+<button
+  class="link--button"
+  on:click={showChart}
+  on:click|once={importChart}
+>{text}</button>
--- src/_bundle/_svelte/data/production.svelte
+++ src/_bundle/_svelte/data/production.svelte
@@ -1,38 +1,43 @@
 <script>
-    import { locale } from '../store'
-    let thisChart;
+  import { locale } from "../store";
+  let thisChart;
 
-    let display = false;
-    let text = $locale === 'zh' ? '顯示圖表 ' : 'Show graph'
+  let display = false;
+  let text = $locale === "zh" ? "顯示圖表 " : "Show graph";
 
-    const toggleClass = () => {
-        if (thisChart === null || thisChart === '' || thisChart === undefined) {
-                thisChart = document.getElementById('productionChart');
-            }
-            thisChart.classList.toggle('hidden');
+  const toggleClass = () => {
+    if (thisChart === null || thisChart === "" || thisChart === undefined) {
+      thisChart = document.getElementById("productionChart");
     }
+    thisChart.classList.toggle("hidden");
+  };
 
-    async function showChart() {
-        display = !display;
-        if (display) {
-            text = $locale === 'zh' ? '隱藏圖表 ' : 'Hide graph'
-        } else {
-            text = $locale === 'zh' ? '顯示圖表 ' : 'Show graph'
-        }
-        toggleClass();
+  async function showChart() {
+    display = !display;
+    if (display) {
+      text = $locale === "zh" ? "隱藏圖表 " : "Hide graph";
+    } else {
+      text = $locale === "zh" ? "顯示圖表 " : "Show graph";
     }
+    toggleClass();
+  }
 
-    async function importChart() {
-        const { default: chart } = await import(
-            "/assets/productionChart.bundle.js"
-        );
-    }
+  async function importChart() {
+    const { default: chart } = await import(
+      "/assets/productionChart.bundle.js"
+    );
+    window.fathom.trackGoal("DTUJVD1F", 0);
+  }
 </script>
 
-<style>
-    .hidden {
-        display: none;
-    }
-</style>
+<button
+  class="link--button"
+  on:click={showChart}
+  on:click|once={importChart}
+>{text}</button>
 
-<button class="link--button" on:click={showChart} on:click|once={importChart}>{ text }</button>
+<style>
+  .hidden {
+    display: none;
+  }
+</style>
