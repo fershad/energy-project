33c5e0c2de6e973156c0cac091ec5dfabb2330cc
fishintaiwan <fershad@gmail.com>
2021-02-15 11:02:35 +0800

    remove staging analytics

--- src/_11ty/shortcode/analytics.js
+++ src/_11ty/shortcode/analytics.js
@@ -1,12 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = async function() {
-    if (process.env.NODE_ENV === 'staging') {
-        return `<!-- Fathom - beautiful, simple website analytics -->
-      <script src="https://heron.cleanenergyproject.tw/script.js" data-site="TMLDWNIR" defer></script>
-      <!-- / Fathom -->`;
-    }
-
     if (dev) {
         return `<!-- Fathom - beautiful, simple website analytics -->`;
     }
