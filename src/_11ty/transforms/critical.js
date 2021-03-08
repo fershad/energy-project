2ed37573754d6325254efbe32e8a6db14715dfc6
fishintaiwan <fershad@gmail.com>
2021-03-08 17:51:45 +0800

    fix critical css bug

--- src/_11ty/transforms/critical.js
+++ src/_11ty/transforms/critical.js
@@ -4,14 +4,16 @@ const path = require('path');
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = async (content, outputPath) => {
-    const styles = path.join(__dirname, `/../../../.cache/css/header.css`);
-    const [{ css: output }] = await new PurgeCSS().purge({
-        content: [{ raw: content, extension: 'html' }],
-        css: [styles],
-        safelist: ['no-js', 'js', 'webp', 'avif', 'link--button', 'hidden'],
-    });
-    const result = `<style>${output}</style>`;
-    const pattern = /<\/title>/s;
+    if (outputPath.endsWith('.html')) {
+        const styles = path.join(__dirname, `/../../../.cache/css/header.css`);
+        const [{ css: output }] = await new PurgeCSS().purge({
+            content: [{ raw: content, extension: 'html' }],
+            css: [styles],
+            safelist: ['no-js', 'js', 'webp', 'avif', 'link--button', 'hidden'],
+        });
+        const result = `<style>${output}</style>`;
+        const pattern = /<\/title>/s;
 
-    return content.replace(pattern, `</title>${result}`);
+        return content.replace(pattern, `</title>${result}`);
+    }
 };
